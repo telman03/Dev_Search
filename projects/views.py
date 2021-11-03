0c8b1d5a825030af9cf45633de37e03378e64c84
telman03 <telmangadimov1@gmail.com>
2021-11-03 23:37:49 +0400

    Adding the Create Update Delete options

--- projects/views.py
+++ projects/views.py
@@ -16,10 +16,29 @@ def createProject(request):
     form = ProjectForm()
 
     if request.method == 'POST':
-        # print(request.POST)
         form = ProjectForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('projects ')
+            return redirect('projects')
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
