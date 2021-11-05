2aa5e5e7e5e8d868bd224b04e5abf0ac4e5bff93
telman03 <telmangadimov1@gmail.com>
2021-11-05 11:55:09 +0400

    Adding default image

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -93,6 +93,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
