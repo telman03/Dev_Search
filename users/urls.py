c7093bcc001d604863f66a1e8d49ffeeb30a3211
telman03 <telmangadimov1@gmail.com>
2021-11-14 16:54:11 +0400

    login/logout page

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-
     path('', views.profiles, name="profiles"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 ]
