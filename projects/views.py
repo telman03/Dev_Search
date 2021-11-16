592162428294841e3a3e25b6067c019f324e1514
telman03 <telmangadimov1@gmail.com>
2021-11-16 21:06:23 +0400

    flash messages

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 
@@ -7,11 +8,12 @@ def projects(request):
     projects = Project.objects.all()
     context = {'projects':projects}
     return render(request, 'projects/projects.html', context)
-
+@login_required(login_url="login")
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-projects.html', {'project': projectObj})
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
 
@@ -23,6 +25,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -34,7 +37,8 @@ def updateProject(request, pk):
             return redirect('projects')
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
-
+    
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    
     if request.method == 'POST':
         username = request.POST.get['username']
         password = request.POST.get['password']
@@ -12,21 +18,21 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print("Username does not exist")
-
+            messages.error(request, "Username does not exist")
+         
         user = authenticate(request, username=username, password=password)
-
 
         if user is not None:
             login(request, user)
             return redirect('profiles')
         else:
-            print("Username OR password is incorrect")
+            messages.error(request, "Username OR password is incorrect")
 
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, "User was logged out!")
     return redirect('login')
 
 def profiles(request):
