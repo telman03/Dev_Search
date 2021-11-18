3a5d7817519306f90bb5018c1f0f6e635e6f0a8f
telman03 <telmangadimov1@gmail.com>
2021-11-18 21:26:38 +0400

    user accounts

--- users/views.py
+++ users/views.py
@@ -40,6 +40,17 @@ def logoutUser(request):
 def registerUser(request):
     page = 'register'
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+
+            messages.success(request, "User was created!")
+
     context = {'page':page, 'form':form}
     return render(request, 'users/login_register.html', context)
 
