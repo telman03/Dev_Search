3cf90faa31e32b967d7c5455724f6a21237f1231
telman03 <telmangadimov1@gmail.com>
2021-11-19 22:20:07 +0400

    Last Commit

--- projects/urls.py
+++ projects/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name="projects"),
-    path('project-object/<str:pk>/', views.project, name="project"),
+    path('project/<str:pk>/', views.project, name="project"),
+
     path('create-project/', views.createProject, name="create-project"),
+
     path('update-project/<str:pk>/', views.updateProject, name="update-project"),
+
     path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
 ]
